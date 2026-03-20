1ef1b6eea98098578f2d86f5f945daff38c1176a
Prajjval Maurya <prajjvalmaurya17@gmail.com>
2026-03-20 20:56:23 +0530

    Update portfolio: remove igloo-clone, add javascript-guide, fix links

--- game1/guessthenumgame.js
+++ game1/guessthenumgame.js
@@ -5,7 +5,7 @@ alert("now you can enter the no. from 0 to 50 to get winning chance");
 let usernum = 21;
 let pro = prompt("guess the number:");
 while (pro!=usernum) {
-    let pro=prompt("you have entered wrong number guess again please:");
+    pro=prompt("you have entered wrong number guess again please:");
 }
 console.log("you have enterd the exact number",pro);
 alert("you win the game");
